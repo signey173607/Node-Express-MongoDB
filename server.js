a2330ea9838710a453afe15f0a043b9f5951afc0
admin <admin@admins-MacBook-Pro.local>
2021-11-03 22:08:34 -0400

    static server 1

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(`Request for ${req.url} by method ${req.method}`);
-    if (req.url === 'GET') {
+
+    if (req.method === 'GET') {
         let fileUrl = req.url;
         if (fileUrl === '/') {
             fileUrl = '/index.html';
@@ -20,27 +21,25 @@ const server = http.createServer((req, res) => {
         if (fileExt === '.html') {
             fs.access(filePath, err => {
                 if (err) {
-                    res.statusCode = '404';
+                    res.statusCode = 404;
                     res.setHeader('Content-Type', 'text/html');
-                    res.end(`<html><body><h1> Error 404: ${fileUrl} not found. </h1></body></html>`);
+                    res.end(`<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`);
                     return;
                 }
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'text/html');
+
                 fs.createReadStream(filePath).pipe(res);
-                // res.end('')
             });
-
         } else {
-            res.statusCode = '404';
+            res.statusCode = 404;
             res.setHeader('Content-Type', 'text/html');
-            res.end(`<html><body><h1> Error 404: ${fileUrl} is not HTML file. </h1></body></html>`);
+            res.end(`<html><body><h1>Error 404: ${fileUrl} is not an HTML file</h1></body></html>`);
         }
-
     } else {
-        res.statusCode = '404';
+        res.statusCode = 404;
         res.setHeader('Content-Type', 'text/html');
-        res.end(`<html><body><h1> Error 404: ${req.method} not supported. </h1></body></html>`);
+        res.end(`<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`);
     }
 });
 
